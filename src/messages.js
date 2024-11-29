0299588e64a9a1b492c2cf11645efc13e6df7227
Sergio Marin <higher.vnf@gmail.com>
2024-11-29 18:14:18 -0300

    fix: correct messages for min lenght and max lenght in array

--- src/messages.js
+++ src/messages.js
@@ -1,9 +1,17 @@
-
 export const defaultMessage = (value, errorType, key, compareWith) =>
-  `${key} need to be ${errorType}. ${value} is invalid`
+	`${key} need to be ${errorType}. ${value} is invalid`;
 
-export const shouldRequired = (value, errorType, key, c) => `${key} is required`
-export const minL = (value, errorType, key, c) => `${key} should less than ${c} and got ${value}`
-export const maxL = (value, errorType, key, c) => `${key} more less than ${c} and got ${value}`
-export const shouldMatch = (value, errorType, key, c) => `${value} should match with ${c}`
-export const shouldEmail = (value, errorType, key, c) => `${value} should be an email`
+export const shouldRequired = (value, errorType, key, c) =>
+	`${key} is required`;
+
+export const minL = (value, errorType, key, c) =>
+	`${key} should be more than ${c} and got ${value}`;
+
+export const maxL = (value, errorType, key, c) =>
+	`${key} should be less than ${c} and got ${value}`;
+
+export const shouldMatch = (value, errorType, key, c) =>
+	`${value} should match with ${c}`;
+
+export const shouldEmail = (value, errorType, key, c) =>
+	`${value} should be an email`;
